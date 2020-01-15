691f4b469330fe5222076dc13c8258b811bd3723
axiomZzz <axiom078@gmail.com>
2020-01-15 15:25:19 +0300

    2

---
+++ app/src/main/java/io/axm/a2pages/Game.java
@@ -0,0 +1,58 @@
+package io.axm.a2pages;
+
+import android.view.View;
+import android.widget.Button;
+
+public class Game {
+    int[] gamePole={1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8};
+    private final int ELEMENTSPOLE=16;
+    public int tempId;
+    public String tempNumb;
+    public int tempId2;
+    public String tempNumb2;
+    boolean flag;
+
+
+    public void selectNUmber( int idButton){
+        switch (idButton){
+            case R.id.Button1:proverka(gamePole[0],R.id.Button1);break;
+            case R.id.Button2:proverka(gamePole[1],R.id.Button2);break;
+            case R.id.Button3:proverka(gamePole[2],R.id.Button3);break;
+            case R.id.Button4:proverka(gamePole[3],R.id.Button4);break;
+            case R.id.Button5:proverka(gamePole[4],R.id.Button5);break;
+            case R.id.Button6:proverka(gamePole[5],R.id.Button6);break;
+            case R.id.Button7:proverka(gamePole[6],R.id.Button7);break;
+            case R.id.Button8:proverka(gamePole[7],R.id.Button8);break;
+            case R.id.Button9:proverka(gamePole[8],R.id.Button9);break;
+            case R.id.Button10:proverka(gamePole[9],R.id.Button10);break;
+            case R.id.Button11:proverka(gamePole[10],R.id.Button11);break;
+            case R.id.Button12:proverka(gamePole[11],R.id.Button12);break;
+            case R.id.Button13:proverka(gamePole[12],R.id.Button13);break;
+            case R.id.Button14:proverka(gamePole[13],R.id.Button14);break;
+            case R.id.Button15:proverka(gamePole[14],R.id.Button15);break;
+            case R.id.Button16:proverka(gamePole[15],R.id.Button16);break;
+
+        }
+
+    }
+    public void proverka(int chislo,int idButton){
+        if (tempNumb==null){
+        tempId=idButton;
+        tempNumb=Integer.toString(chislo);
+        } else{
+            tempId2=idButton;
+            tempNumb2=Integer.toString(chislo);
+            if (tempNumb==tempNumb2){
+                flag=true;
+            } else {
+                flag=false;
+            }
+            tempNumb=null;
+        }
+
+
+
+
+
+    }
+}
--- app/src/main/java/io/axm/a2pages/MainActivity.java
+++ app/src/main/java/io/axm/a2pages/MainActivity.java
@@ -3,12 +3,102 @@ package io.axm.a2pages;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+    Game game;
+    Button button;
+    Timer timer=new Timer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        game=new Game();
+
+        final int [] numbersId={
+                R.id.Button1,
+                R.id.Button2,
+                R.id.Button3,
+                R.id.Button4,
+                R.id.Button5,
+                R.id.Button6,
+                R.id.Button7,
+                R.id.Button8,
+                R.id.Button9,
+                R.id.Button10,
+                R.id.Button11,
+                R.id.Button12,
+                R.id.Button13,
+                R.id.Button14,
+                R.id.Button15,
+                R.id.Button16,
+        };
+        View.OnClickListener buttonListener=new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    game.selectNUmber(v.getId());
+
+                    if (game.tempNumb2 == null) {
+
+                        button = findViewById(game.tempId);
+                        button.setText(game.tempNumb);
+                    } else if (game.tempNumb2 != null) {
+
+                        button = findViewById(game.tempId2);
+                        button.setText(game.tempNumb2);
+
+                        if(game.tempNumb==game.tempNumb2){
+                           // game.tempNumb=null;
+
+                        }
+                        else{
+                            button = findViewById(game.tempId);
+                            button.setText("X");
+
+                            game.tempNumb=game.tempNumb2;
+                            game.tempId=game.tempId2;
+
+                        }
+
+
+                        /*timer.schedule(new TimerTask() {
+                            @Override
+                            public void run() {
+                                try {
+                                    if (game.tempNumb != game.tempNumb2) {
+
+
+                                        button = findViewById(game.tempId);
+                                        button.setText("X");
+
+                                        button = findViewById(game.tempId2);
+                                        button.setText("X");
+                                    }
+                                } catch (Exception e){
+
+                                }
+
+                            }
+                        },1000);*/
+
+
+                    }
+
+
+            }
+        };
+        for (int i = 0; i <numbersId.length ; i++) {
+            findViewById(numbersId[i]).setOnClickListener(buttonListener);
+
+        }
+
+
     }
 }
